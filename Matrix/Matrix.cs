6f1b298d069fe8382f3dafbb4a36b786b3966374
FiveMountainTomoto <2257485855@qq.com>
2024-07-18 11:41:03 +0800

    初始提交

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+
+namespace Matrix
+{
+    public class Matrix : IEnumerable
+    {
+        private double[,] _arr;
+        #region 索引器、行列属性
+        public int RowsCount => _arr.GetLength(0);
+        public int ColumnsCount => _arr.GetLength(1);
+        public double this[int row, int col]
+        {
+            get => _arr[row, col];
+            set => _arr[row, col] = value;
+        }
+        #endregion
+        #region 构造函数
+        public Matrix(int row, int col) => _arr = new double[row, col];
+        public Matrix(double[,] arr) => _arr = arr;
+        #endregion
+        #region 实现IEnumerable接口
+        public IEnumerator GetEnumerator() => _arr.GetEnumerator();
+        #endregion
+        #region 矩阵加减乘运算、转置
+        public Matrix Multiply(Matrix other)
+        {
+            if (ColumnsCount != other.RowsCount) throw new ArgumentException($"矩阵的列数 {ColumnsCount} 不等于另一矩阵的行数 {other.RowsCount}");
+            Matrix result = new Matrix(RowsCount, other.ColumnsCount);
+            for (int i = 0; i < RowsCount; i++)
+            {
+                for (int j = 0; j < other.ColumnsCount; j++)
+                {
+                    for (int e = 0; e < ColumnsCount; e++)
+                    {
+                        result[i, j] += this[i, e] * other[e, j];
+                    }
+                }
+            }
+            return result;
+        }
+        public Matrix Add(Matrix other)
+        {
+            if (RowsCount != other.RowsCount || ColumnsCount != other.ColumnsCount) throw new ArgumentException($"矩阵的行列数 {RowsCount},{ColumnsCount} 不等于另一矩阵的行列数 {other.RowsCount},{other.ColumnsCount}");
+            Matrix result = new Matrix(RowsCount, ColumnsCount);
+            for (int i = 0; i < RowsCount; i++)
+            {
+                for (int j = 0; j < ColumnsCount; j++)
+                {
+                    result[i, j] = this[i, j] + other[i, j];
+                }
+            }
+            return result;
+        }
+        public Matrix Transpose()
+        {
+            Matrix result = new Matrix(ColumnsCount, RowsCount);
+            for (int i = 0; i < RowsCount; i++)
+            {
+                for (int j = 0; j < ColumnsCount; j++)
+                {
+                    result[j, i] = this[i, j];
+                }
+            }
+            return result;
+        }
+        public static Matrix operator -(Matrix m)
+        {
+            Matrix result = new Matrix(m.RowsCount, m.ColumnsCount);
+            for (int i = 0; i < m.RowsCount; i++)
+            {
+                for (int j = 0; j < m.ColumnsCount; j++)
+                {
+                    result[i, j] = -m[i, j];
+                }
+            }
+            return result;
+        }
+        public static Matrix operator -(Matrix m1, Matrix m2) => m1.Add(-m2);
+        public static Matrix operator +(Matrix m1, Matrix m2) => m1.Add(m2);
+        public static Matrix operator *(Matrix m1, Matrix m2) => m1.Multiply(m2);
+        #endregion
+    }
+}
--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,15 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Matrix
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            double[,] _m1 = new double[,]
+            {
+                {1,2,3 },
+                {2,3,4 },
+                {3,4,5 }
+            };
+            double[,] _m2 = new double[,]
+            {
+                {1,1,4 },
+                {5,1,4 },
+                {1,9,1 }
+            };
+            double[,] _m3 = new double[,]
+            {
+                {1, 1},
+                {4,5 },
+                {1,4 }
+            };
+            double[,] _m4 = new double[,]
+            {
+                {1,9,1 ,9},
+                {8,1,0 ,0},
+            };
+            Matrix m1 = new Matrix(_m1);
+            Matrix m2 = new Matrix(_m2);
+            Matrix m3 = new Matrix(_m3);
+            Matrix m4 = new Matrix(_m4);
+            Console.WriteLine("m1,m2,m3,m4");
+            PrintMatrix(m1);
+            PrintMatrix(m2);
+            PrintMatrix(m3);
+            PrintMatrix(m4);
+            Console.WriteLine("m1+m2");
+            PrintMatrix(m1 + m2);
+            Console.WriteLine("m1-m2");
+            PrintMatrix(m1 - m2);
+            Console.WriteLine("m3*m4");
+            PrintMatrix(m3 * m4);
+            Console.WriteLine("m4^T");
+            PrintMatrix(m4.Transpose());
+            Console.WriteLine("m2+m3");
+            try
+            {
+                PrintMatrix(m2 + m3);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("m1*m4");
+            try
+            {
+                PrintMatrix(m1 * m4);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+        private static void PrintMatrix(Matrix m)
+        {
+            for (int i = 0; i < m.RowsCount; i++)
+            {
+                for (int j = 0; j < m.ColumnsCount; j++)
+                {
+                    Console.Write(m[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
